c475977dca35d2364be90ee33df496211674caf4
Philipp Fleck <philipp.fleck@fh-hagenberg.at>
2022-01-25 10:30:06 +0100

    changed to BigInt

--- Program.cs
+++ Program.cs
@@ -1,31 +1,31 @@
 ﻿using System;
+using System.Numerics;
 
 namespace Fibonacci {
     static class Program {
         static void Main(string[] args) {
             int num = 5;
-
-            if (args.Length == 1) {
+            if (args.Length == 1)
                 num = int.Parse(args[0]);
-            }
 
-            int[] fibonacci = CreateFibonacciSeries(num);
+            BigInteger[] fibonacci = CreateFibonacciSeries(num);
 
             for (int i = 0; i < fibonacci.Length; i++) {
                 Console.Out.Write(fibonacci[i]);
                 Console.Out.Write(" ");
             }
+
             Console.Out.WriteLine();
         }
 
-        static int CreateFibonacciNumber(int index) {
+        static BigInteger CreateFibonacciNumber(int index) {
             if (index == 0) return 0;
             if (index == 1) return 1;
             return CreateFibonacciNumber(index - 1) + CreateFibonacciNumber(index - 2);
         }
 
-        static int[] CreateFibonacciSeries(int count) {
-            int[] fibonacciSeries = new int[count];
+        static BigInteger[] CreateFibonacciSeries(int count) {
+            BigInteger[] fibonacciSeries = new BigInteger[count];
             for (int i = 0; i < count; i++)
                 fibonacciSeries[i] = CreateFibonacciNumber(i);
             return fibonacciSeries;
